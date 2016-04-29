6f1d47c6dd277bbf1201d190dde69de195757fe9
therealplato <therealplato@gmail.com>
2016-04-28 23:14:14 -0400

    Load api/profile/:id from angular

--- web/leadsApp.js
+++ web/leadsApp.js
@@ -1,10 +1,18 @@
 angular.module('LeadsApp', ['ngMaterial'])
 .controller('oneProfileController', [
-  '$scope',
   '$element',
-  function($scope, $element){
-    els = $element.find("meta");
-    console.log(els[0])
-    $scope.profile = {id: els[0].dataset['profileid']};
+  '$http',
+  '$scope',
+  function($element, $http, $scope){
+    var els = $element.find("meta");
+		var id = els[0].dataset['profileid'];
+		$http({
+			method: 'GET',
+			url: '/api/profile/'+id,
+		}).then(function successCallback(response) {
+			console.log(response)
+			$scope.profile = response.data;
+		}, function errorCallback(response) {
+		});
   }]
 )
